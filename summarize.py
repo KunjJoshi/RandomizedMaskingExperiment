8c689c09e2f1b2b0e9cb525332c451864df7092e
KunjJoshi <iamkunjjoshi@gmail.com>
2026-05-03 16:50:33 -0400

    Print Statements

--- summarize.py
+++ summarize.py
@@ -35,13 +35,38 @@ base_df = pd.read_csv(base_path)
 rmft_df = pd.read_csv(rmft_path)
 dedup_df = pd.read_csv(dedup_path)
 
-base_leaks = set(base_df['leaked_email'].to_list()) & ORIGINAL_SET
-rmft_leaks = set(rmft_df['leaked_email'].to_list()) & ORIGINAL_SET
-dedup_leaks = set(dedup_df['leaked_email'].to_list()) & ORIGINAL_SET
+base_leaks = set(base_df['leaked_email'].to_list())
+rmft_leaks = set(rmft_df['leaked_email'].to_list())
+dedup_leaks = set(dedup_df['leaked_email'].to_list())
 
-base_logprobs = base_df['logprob'].to_list()
-rmft_logprobs = rmft_df['logprob'].to_list()
-dedup_logprobs = dedup_df['logprob'].to_list()
+base_logprobs_og = base_df[base_df['leaked_email'] in (base_leaks & ORIGINAL_SET)]['logprob'].to_list()
+rmft_logprobs_og = rmft_df[rmft_df['leaked_email'] in (rmft_leaks & ORIGINAL_SET)]['logprob'].to_list()
+dedup_logprobs_og = dedup_df[dedup_df['leaked_email'] in (dedup_leaks & ORIGINAL_SET)]['logprob'].to_list()
+
+base_logprobs_fake = base_df[base_df['leaked_email'] in (base_leaks - ORIGINAL_SET)]['logprob'].to_list()
+rmft_logprobs_fake = rmft_df[rmft_df['leaked_email'] in (rmft_leaks - ORIGINAL_SET)]['logprob'].to_list()
+dedup_logprobs_fake = dedup_df[dedup_df['leaked_email'] in (dedup_leaks - ORIGINAL_SET)]['logprob'].to_list()
+
+if len(base_logprobs_og) > 0 and len(base_logprobs_fake) > 0:
+    avg_og = sum(base_logprobs_og)/len(base_logprobs_og)
+    avg_fake = sum(base_logprobs_fake)/len(base_logprobs_fake)
+    base_logdiff = avg_og - avg_fake
+else:
+    base_logdiff = 0
+
+if len(rmft_logprobs_og) > 0 and len(rmft_logprobs_fake) > 0:
+    avg_og = sum(rmft_logprobs_og)/len(rmft_logprobs_og)
+    avg_fake = sum(rmft_logprobs_fake)/len(rmft_logprobs_fake)
+    rmft_logdiff = avg_og - avg_fake
+else:
+    rmft_logdiff = 0
+
+if len(dedup_logprobs_og) > 0 and len(dedup_logprobs_fake) > 0:
+    avg_og = sum(dedup_logprobs_og)/len(dedup_logprobs_og)
+    avg_fake = sum(dedup_logprobs_fake)/len(dedup_logprobs_fake)
+    dedup_logdiff = avg_og - avg_fake
+else:
+    dedup_logdiff = 0
 
 prompts = pd.read_csv("../datasets/enron/discoverable_prompts.csv")['prompt'].to_list()
 
@@ -55,9 +80,52 @@ print(f"\n RMFT Email Leaks: {(len(rmft_leaks)/len(ORIGINAL_SET))*100}")
 print(f"\n Dedup Email Leaks: {(len(dedup_leaks)/len(ORIGINAL_SET))*100}")
 
 
-print(f"Average Logprob Confidence per generation for Baseline Finetuning: {sum(base_logprobs)/len(base_logprobs)}")
-print(f"Average Logprob Confidence per generation for RMFT: {sum(rmft_logprobs)/len(rmft_logprobs)}")
-print(f"Average Logprob Confidence per generation for Deduplication: {sum(dedup_logprobs)/len(dedup_logprobs)}")
+if base_logdiff > 0:
+    base_ps = f"Baseline FT prefers Original Emails over Fake emails with Logdiff: {base_logdiff}"
+    base_ps += f"\n Average LogDiff on Original Emails: {sum(base_logprobs_og)/len(base_logprobs_og)}"
+    base_ps += f"\n Average LogDiff on Fake Emails: {sum(base_logprobs_fake)/len(base_logprobs_fake)}"
+elif base_logdiff < 0:
+    base_ps = f"Baseline FT prefers Fake Emails over Original emails with Logdiff: {base_logdiff}"
+    base_ps += f"\n Average LogDiff on Original Emails: {sum(base_logprobs_og)/len(base_logprobs_og)}"
+    base_ps += f"\n Average LogDiff on Fake Emails: {sum(base_logprobs_fake)/len(base_logprobs_fake)}"
+else:
+    base_ps = f"Baseline FT did not generate one kind of emails at all"
+    base_ps += f"Length of Original Emails generated: {len(base_logprobs_og)}"
+    base_ps += f"Length of Fake Emails generated: {len(base_logprobs_fake)}"
+
+if rmft_logdiff > 0:
+    rmft_ps = f"RMFT prefers Original Emails over Fake emails with Logdiff: {rmft_logdiff}"
+    rmft_ps += f"\n Average LogDiff on Original Emails: {sum(rmft_logprobs_og)/len(rmft_logprobs_og)}"
+    rmft_ps += f"\n Average LogDiff on Fake Emails: {sum(rmft_logprobs_fake)/len(rmft_logprobs_fake)}"
+elif rmft_logdiff < 0:
+    rmft_ps = f"RMFT prefers Fake Emails over Original emails with Logdiff: {rmft_logdiff}"
+    rmft_ps += f"\n Average LogDiff on Original Emails: {sum(rmft_logprobs_og)/len(rmft_logprobs_og)}"
+    rmft_ps += f"\n Average LogDiff on Fake Emails: {sum(rmft_logprobs_fake)/len(rmft_logprobs_fake)}"
+else:
+    rmft_ps = f"RMFT did not generate one kind of emails at all"
+    rmft_ps += f"Length of Original Emails generated: {len(rmft_logprobs_og)}"
+    rmft_ps += f"Length of Fake Emails generated: {len(rmft_logprobs_fake)}"
+
+if dedup_logdiff > 0:
+    dedup_ps = f"Dedup FT prefers Original Emails over Fake emails with Logdiff: {dedup_logdiff}"
+    dedup_ps += f"\n Average LogDiff on Original Emails: {sum(dedup_logprobs_og)/len(dedup_logprobs_og)}"
+    dedup_ps += f"\n Average LogDiff on Fake Emails: {sum(dedup_logprobs_fake)/len(dedup_logprobs_fake)}"
+elif dedup_logdiff < 0:
+    dedup_ps = f"Dedup FT prefers Fake Emails over Original emails with Logdiff: {dedup_logdiff}"
+    dedup_ps += f"\n Average LogDiff on Original Emails: {sum(dedup_logprobs_og)/len(dedup_logprobs_og)}"
+    dedup_ps += f"\n Average LogDiff on Fake Emails: {sum(dedup_logprobs_fake)/len(dedup_logprobs_fake)}"
+else:
+    dedup_ps = f"Dedup FT did not generate one kind of emails at all"
+    dedup_ps += f"Length of Original Emails generated: {len(dedup_logprobs_og)}"
+    dedup_ps += f"Length of Fake Emails generated: {len(dedup_logprobs_fake)}"
+
+print(f"\n===========SUMMARY OF AVG LOGDIFF=========\n")
+print("Baseline Finetuning\n ")
+print(base_ps)
+print("RMFT \n")
+print(rmft_ps)
+print("Dedup \n")
+print(dedup_ps)
 
 print(f"Number of prompts that leaked Emails for Baseline Finetuning: {len(base_leak_prompts)}/{len(prompts)}")
 print(f"Number of prompts that leaked Emails for RMFT: {len(rmft_leak_prompts)}/{len(prompts)}")
